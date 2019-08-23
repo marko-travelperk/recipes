3208f31459badae843febc70df4c5beaa2791ec9
Marko <marko.milanovic@travelperk.com>
2019-08-23 10:43:46 +0200

    Initial implementation, no tests

---
+++ polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'polls', views.PollsViewSet)
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/marko/PycharmProjects/recipes/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
