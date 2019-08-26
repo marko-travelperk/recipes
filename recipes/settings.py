104908ecf94dca06d45fc46138d2f6c0b992beeb
Marko <marko.milanovic@travelperk.com>
2019-08-26 10:32:36 +0200

    squashing migrations

--- recipes/settings.py
+++ recipes/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'recipesapp.apps.RecipesappConfig',
+    #'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
